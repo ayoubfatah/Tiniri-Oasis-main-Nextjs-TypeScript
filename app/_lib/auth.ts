fcacb4876dc5a9f7ff2dbd9d2ba57e0081ebf1db
ayoubfatah <ayoubfatah222@gmail.com>
2024-06-02 12:26:08 +0100

    creating new guest on the supabase server when singin up

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import google from 'next-auth/providers/google';
 import GoogleProvider from 'next-auth/providers/google'; //we import the provider that we want
+import { createGuest, getGuest } from './data-service';
 
 const autConfig = {
    providers: [
@@ -13,9 +14,30 @@ const autConfig = {
       authorized({ auth, request }: any) {
          return !!auth?.user;
       },
+      async signIn({ user, account, profile }: any) {
+         try {
+            // checking if the guest exist
+            const existingGuest = await getGuest(user.email);
+
+            if (!existingGuest) {
+               await createGuest({ email: user.email, fullName: user.name });
+            }
+            return true;
+         } catch (error) {
+            return false;
+         }
+      },
+      async session({ session, user }: any) {
+         const guest = await getGuest(session.user.email);
+         session.user.guestId = guest.id;
+         return session;
+      },
    },
+   pages: { signIn: '/login' },
 };
 export const {
    auth,
+   signIn,
+   signOut,
    handlers: { GET, POST },
 } = NextAuth(autConfig);
