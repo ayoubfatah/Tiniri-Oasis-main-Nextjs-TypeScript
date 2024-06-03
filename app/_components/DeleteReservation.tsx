68adb51a20cfb71cb7b4d2a98bf6b725d8a404c7
ayoubfatah <ayoubfatah222@gmail.com>
2024-06-03 21:47:42 +0100

    adding loading indicator using  useTransition hook

--- app/_components/DeleteReservation.tsx
+++ app/_components/DeleteReservation.tsx
@@ -1,14 +1,21 @@
 'use client';
 import { TrashIcon } from '@heroicons/react/24/solid';
 import { deleteReservationAction } from '../_lib/action';
+import { useTransition } from 'react';
 
 type Prop = {
    bookingId: any;
 };
 function DeleteReservation({ bookingId = '' }: Prop) {
+   const [isPending, startTransition] = useTransition();
+   function handleDelete() {
+      if (confirm('Are you sure you want to delete this reservation?'))
+         startTransition(() => deleteReservationAction(bookingId));
+   }
+   console.log(isPending);
    return (
       <button
-         onClick={() => deleteReservationAction(bookingId)}
+         onClick={handleDelete}
          className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
       >
          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
