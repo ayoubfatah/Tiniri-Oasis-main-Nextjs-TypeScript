999c391c1c09c463d29f539f5676ec77948adbbd
ayoubfatah <ayoubfatah222@gmail.com>
2024-06-03 20:49:34 +0100

    deleting reservations , protecting our data if anyone tried to delete bookings using curl

--- app/_components/DeleteReservation.tsx
+++ app/_components/DeleteReservation.tsx
@@ -1,16 +1,20 @@
+'use client';
 import { TrashIcon } from '@heroicons/react/24/solid';
+import { deleteReservationAction } from '../_lib/action';
 
 type Prop = {
-  bookingId: any;
+   bookingId: any;
 };
 function DeleteReservation({ bookingId = '' }: Prop) {
-  console.log(bookingId);
-  return (
-    <button className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900">
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
-    </button>
-  );
+   return (
+      <button
+         onClick={() => deleteReservationAction(bookingId)}
+         className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+      >
+         <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+         <span className="mt-1">Delete</span>
+      </button>
+   );
 }
 
 export default DeleteReservation;
--- app/_lib/action.ts
+++ app/_lib/action.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
-import { updateGuest } from './data-service';
+import { getBookings, updateGuest } from './data-service';
 import { supabase } from './supabase';
 
 export async function signInAction() {
@@ -14,7 +14,7 @@ export async function signOutAction() {
 
 export async function updateGuestAction(formData: any) {
    const session = await auth();
-   console.log(session);
+
    if (!session) throw new Error('Not authorized');
    const NationalID = formData.get('NationalID');
    const nationality = formData.get('nationality');
@@ -33,4 +33,24 @@ export async function updateGuestAction(formData: any) {
 
    if (error) throw new Error('Guest could not be updated');
    revalidatePath('/account/profile');
+}
+
+export async function deleteReservationAction(bookingId: string | number) {
+   const session = await auth();
+
+   if (!session) throw new Error('Not authorized');
+   const guestBookings = await getBookings(session.user.guestId);
+   const guestBookingsIds = guestBookings.map((booking: any) => booking.id);
+   if (bookingId && !guestBookingsIds.includes(bookingId))
+      throw new Error('Booking not found');
+
+   
+   const { error } = await supabase
+      .from('bookings')
+      .delete()
+      .eq('id', bookingId);
+
+   if (error) throw new Error('Booking could not be deleted');
+
+   revalidatePath('/account/reservations');
 }
